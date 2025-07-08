f321a65f4e00f280b09e56daa7cf1cec359dc3ed
Kailashnath0406 <kailashnadh0406@gmail.com>
2025-07-09 01:12:25 +0530

    initial Draft Kailashnath: Implemented JWT Authentication with update, delete,list and get APIs

--- src/main/java/com/table2table/service/CustomUserDetailsService.java
+++ src/main/java/com/table2table/service/CustomUserDetailsService.java
@@ -1,14 +1,17 @@
 package com.table2table.service;
 
+import com.table2table.dto.CustomUserDetails;
 import com.table2table.model.User;
 import com.table2table.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,14 +20,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository; // Or UserService if you prefer
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                new ArrayList<>() // Add roles/authorities here if needed
-        );
+        user.setRole("ROLE_"+user.getRole());
+        return new CustomUserDetails(user); // ✅ injects role like "ADMIN"
     }
 }
