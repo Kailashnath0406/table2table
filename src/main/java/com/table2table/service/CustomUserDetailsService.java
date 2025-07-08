52f93aaedfc0bc22ccedcb31f2935901f98eb31e
Kailashnath0406 <73091815+Kailashnath0406@users.noreply.github.com>
2025-07-08 21:00:52 +0530

    Conflict resolution and sync dev and prod
    * Add .gitignore for frontend and backend (#1)
    
    * Update .gitignore (#2)
    
    * initial Draft Kailashnath: Implemented JWT Authentication with Register, Login, and Profile APIs (#6)

---
+++ src/main/java/com/table2table/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.table2table.service;
+
+import com.table2table.model.User;
+import com.table2table.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository; // Or UserService if you prefer
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>() // Add roles/authorities here if needed
+        );
+    }
+}
